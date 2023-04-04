6276d2d6ffbcae327d8caa7e1333e861b7fd018a
benfro21 <broad0601@gmail.com>
2023-04-04 13:02:14 -0400

    folderStructure

---
+++ sandbox/warmUps.js
@@ -0,0 +1,22 @@
+
+//warm up 4/4/23
+// Write a block of code that will print out the largest value.
+// Using the given values as an example, your code should print out 2.
+// To verify your code is working properly, try and change the values of x and y.
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+let maxOfTwo = (x,y) => {
+    if( x === y){
+        console.log('The values are identical')
+    }
+    else if(x > y){
+      console.log(x)  
+    }else {
+        console.log(y)
+    }
+}
+// maxOfTwo(5,57)
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
