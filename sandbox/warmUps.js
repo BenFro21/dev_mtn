c7bb4caaf1c863a3069e0a792e3cfc52ec084f45
benfro21 <broad0601@gmail.com>
2023-04-10 10:30:35 -0400

    morning warmup

--- sandbox/warmUps.js
+++ sandbox/warmUps.js
@@ -41,18 +41,47 @@ let maxOfTwo = (x,y) => {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Starting array
+// ```js
+// let array = [28, 43, -12, 30, 4, 0, -36]
+// // Write your solution below:
+// let largest = 0
+// let smallest = 0
+// for(i = 0; i < array.length; i++){
+//     if(array[i] > largest){
+//         largest = array[i]
+//     }
+//     if(array[i] < smallest){
+//         smallest = array[i]
+//     }
+// }
+// console.log(largest, smallest)
+// ```
+
+////////////////////////////////////////////////////////////////////
+
+// Given an array of numbers, console.log true if any two numbers in the array add to zero, otherwise log false.
+// For example:
+// [1, 4, 11, 2, 37, -4] should log true because 4 and -4 add to 0.
+// [0, 21, 33, 6, 0, -9] should log true because 0 and 0 add to 0.
+// [0, 1, 2, 3, 4, 5] should log false because no two numbers add to 0.
+
+
 ```js
-let array = [28, 43, -12, 30, 4, 0, -36]
-// Write your solution below:
-let largest = 0
-let smallest = 0
-for(i = 0; i < array.length; i++){
-    if(array[i] > largest){
-        largest = array[i]
+let arrZero = (arr) => {
+    let value = false;
+    for(let i =0; i < arr.length; i++){
+      for(let j=0; j < arr.length; j++){
+        if( i !== j){
+            if(arr[i] + arr[j] === 0){
+                value = true
+            }
+        }
+      }
     }
-    if(array[i] < smallest){
-        smallest = array[i]
-    }
+    return value
 }
-console.log(largest, smallest)
+
+console.log(arrZero([1, 4, 11, 2, 37, -4]))
+console.log(arrZero([0, 21, 33, 6, 0, -9]))
+console.log(arrZero([0, 1, 2, 3, 4, 5]))
 ```
