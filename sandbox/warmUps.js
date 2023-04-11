fd78a73279afbfd5796b5da0aabd1fb06f5853a2
benfro21 <broad0601@gmail.com>
2023-04-11 11:16:39 -0400

    warm-up_4_11

--- sandbox/warmUps.js
+++ sandbox/warmUps.js
@@ -85,7 +85,9 @@ let maxOfTwo = (x,y) => {
 // console.log(arrZero([0, 21, 33, 6, 0, -9]))
 // console.log(arrZero([0, 1, 2, 3, 4, 5]))
 // ```
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////\
+//Tuesday 4/11
+
 // Given a number, n, return an array containing n unique random numbers between 1-10, inclusive. (That is, do not repeat any numbers in the returned list.)
 // You can trust that this function will never be called with n < 0 or n > 10.
 // For example:
@@ -94,6 +96,17 @@ let maxOfTwo = (x,y) => {
 // luckyNumbers(6)
 // returns (1, 7, 9, 6, 5, 2)
 
+// ```js
+// numsArr = [];
+// let luckNumbers = (n) => {
+//     for(let i=0; i < n; i++){
+//         let randomNumber = Math.floor(Math.random() * n)
+//         numsArr.push(randomNumber)
+//     }
+//     return numsArr
+// }
+// console.log(luckNumbers(8))
+// ```
 
 
 
