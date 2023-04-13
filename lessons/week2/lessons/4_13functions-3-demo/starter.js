bb8f7369d4c8ad20c4efb7c86c8d079a23a4bc5b
benfro21 <broad0601@gmail.com>
2023-04-13 13:49:03 -0400

    lecture

--- lessons/week2/lessons/4_13functions-3-demo/starter.js
+++ lessons/week2/lessons/4_13functions-3-demo/starter.js
@@ -11,22 +11,16 @@ const copyArrToCamelCase = arr => {
         const str = arr[i]
         const splitStr = str.split(' ')
         let camelCaseStr = ''
-        
         for (let x = 0; x < splitStr.length; x++) {
             let word = splitStr[x]
-
             word = word.toLowerCase()
-
             if (x !== 0) {
                 word = word.charAt(0).toUpperCase() + word.slice(1)
             }
-
             camelCaseStr += word
         }
-
         newArr.push(camelCaseStr)
     }
-
     return newArr
 }
 
@@ -45,113 +39,134 @@ const copyArrToSnakeCase = arr => {
 }
   
 // CODE HERE
+//higher order function below 
+const copyArrAndChange = (arr, cb) => {
+    let result = [];
+    for(let i=0; i< arr.length; i++){
+        let newValue = cb(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+//callbacks for above higher order function 
+const copyToCamelCaseCB = str => {
+    const splitStr = str.split(' ')
+    let camelCaseStr = ''
+    for(let i=0; i < splitStr.length; i++){
+        let word = splitStr[i]
+        word = word.toLowerCase()
+        if(i !== 0){
+            word = word.charAt(0).toUpperCase() + word.slice(1)
+        }
+        camelCaseStr += word
+    }
+    return camelCaseStr
+}
+const copyStrToSnakeCaseCb = str => {
+    str = str.toLowerCase().split(' ').join('_')
+    return str
+}
+
 
 
 ////////////////////////////////////////
 ////// HIGHER ORDER ARRAY METHODS //////
-////////////////////////////////////////
-
-
 //// MAP ////
-
 /*
     Pass a callback to map that will return 'pink'
     for each color in the array.
 */
-
 const colors = ['red', 'blue', 'yellow', 'green', 'orange']
 
-const mappedColors // = colors.map()
+// let turnToPink = () => 'pink'
+// const mappedColors  = colors.map(turnToPink)
+// console.log(mappedColors)
+const mappedColorss = colors.map((el) => {
+    let primary = ['red', 'yellow', 'blue']
+    let isPrimary = primary.includes(el)
+    return {
+        'color': el,
+        'isPrimary': isPrimary
+    }
+})
+// console.log(mappedColorss)
+
 
 /*
     Edit the formalGreeting function and use the built in .map method 
     to map over the names parameter and return a new array with "Hello, " 
     appended to the beginning of each name
-    
     Make sure to use arrow functions combined with the map method    
 */
-
 const formalNames = ['Bernard', 'Elizabeth', 'Conrad', 'Mary Margaret']
-
 const formalGreeting = names => {
     // CODE HERE
+   return names.map(name => `Hello, ${name}`)
 }
-
 // Call formalGreeting passing in the formalNames array
-
+console.log(formalGreeting(formalNames))
 
 //// FILTER ////
-
 /*
     Pass a callback to filter that will return
     only strings that begin with the letter A
 */
-
 const places = ['Binghampton', 'Albany', 'New York', 'Ithaca', 'Auburn', 'Rochester', 'Buffalo']
-
-const placesThatStartWithA // = places.filter()
-
+const placesThatStartWithA = places.filter(city => city.charAt(0) === 'A')
+console.log(placesThatStartWithA)
 
 /*
     Create a function called identifier that uses the filter higher order 
     array method to filter over the provided jobs array of objects
-
     The function should return the object of the person with a job as a programmer
-    
     Make sure to use the arrow function in conjunction with the filter method
-    
     Your returned value should be a single object, not an array with one object inside of it
-    
     Use arrow functions and the filter method
 */
-
 // Do not edit the code below.
 let jobs = [
 	{ receptionist: "James" },
 	{ programmer: "Steve" },
 	{ designer: "Alicia" },
 ];
-
 // Do not edit the code above.
-
 // CODE HERE
+const identifer = arr => {
+   return arr.filter(el => el.programmer)[0]
+}
+
+console.log(identifer(jobs))
+
 
 // call the function passing in the jobs array
-
-
 //// REDUCE ////
-
 /*
     Edit the productOfArray function and use 
     the built in .reduce method to loop over the numbers parameter
     and return the product of all the numbers in the array
-
     Make sure to use arrow functions combined with the reduce method    
 */
-
 const numsToReduce = [43, 7, 24, 79, 290]
-
 const productOfArray = numbers => {
-    // Code here
-}
+        // Code here
+   return numbers.reduce((acc, curr) => acc * curr)
 
+}
+console.log(productOfArray(numsToReduce))
 // CODE HERE
 
 
+
 // call productOfArray passing in numsToReduce
-
-
 /*
     Pass a callback and an initial value to reduce 
     that will subtract all the expenses in the array
     from the initial budget
-
     This will allow us to see how much we have left
     in the budget after these expenses
 */
-
 const budget = 2000
-
 const expenses = [
     {
         title: 'rent', 
@@ -166,5 +181,5 @@ const expenses = [
         amount: 300
     }
 ]
-
-const remaining // = expenses.reduce(//callback, //initial value)
+const remaining  = expenses.reduce((a, c) => a - c.amount, budget)
+console.log(remaining)
