6baf668d7c5a32295a9bb94c2eda57efad343790
benfro21 <broad0601@gmail.com>
2023-04-26 10:08:28 -0400

    code challenge 4_26

--- sandbox/warmUps.js
+++ sandbox/warmUps.js
@@ -316,3 +316,21 @@ let maxOfTwo = (x,y) => {
 // console.log(removeDuplicateLetters('helloworld'))
 // console.log(removeDuplicateLetters('iwanttoclimbamoutain'))
 // ```
+/////////////////////////////////////////////////
+///////// 4/26
+let hasBalancedParentheses = (str) => {
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === '('){
+            count++;   
+        } else if(str[i] === ')'){
+            count--;
+            if(count < 0){
+                return false;
+            }
+        }
+    }
+    return count === 0;
+} 
+
+console.log(hasBalancedParentheses('((()))))'))
