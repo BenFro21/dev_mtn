c4c7a54a9ce2204eae1b3aa880faf46e0a4568ad
benfro21 <broad0601@gmail.com>
2023-05-09 16:45:49 -0400

    finished warm up demo and lab 5-9

--- sandbox/warmUps.js
+++ sandbox/warmUps.js
@@ -485,7 +485,7 @@ let maxOfTwo = (x,y) => {
 //     });
 //     return result
 // }
-console.log(guessGifts(wishlist, presents)); // must return ["Toy Car", "Mini Puzzle"]
+// console.log(guessGifts(wishlist, presents)); // must return ["Toy Car", "Mini Puzzle"]
 ///////////5/8//////////////////////////////////////////////////////////////////////////////////////////
 
 // let towerBuilder = (n) => {
@@ -505,3 +505,30 @@ console.log(guessGifts(wishlist, presents)); // must return ["Toy Car", "Mini Pu
 // } 
 
 // console.log(towerBuilder(5))
+///////////////////////5/9////////////////////////////////////////
+// You might know some pretty large perfect squares. But what about the NEXT one?
+// Write a function in perfectSquare.js that finds the next perfect square after the one passed in as an argument and returns it.
+// Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer (look at the examples if this sounds confusing).
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+// For example:
+// perfectSquare(9) // should return 16 (3x3=9, 4x4=16)
+// perfectSquare(289) // should return 324 (17x17=289 18x18=324)
+// perfectSquare(3000) // should return -1 (sq root of 3000 is 54.77)
+
+
+const perfectSquare = (num) => {
+    // calculate the square root of the given number
+    const squareRoot = Math.sqrt(num);
+    // if the square root is an integer, then it is a square. 
+    if(Number.isInteger(squareRoot)){
+        // find the next perfect square by adding 1 to the square root and then squareing it 
+        const nextSquare = Math.pow(squareRoot +1, 2)
+        return nextSquare
+    }else{
+        // if the square root is not an integer, then the number is not a perfect square 
+        return -1
+    }
+}
+console.log(perfectSquare(9))
+console.log(perfectSquare(289))
+console.log(perfectSquare(3000))
